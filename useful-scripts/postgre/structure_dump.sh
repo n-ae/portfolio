25a410eb0fce419697855b90601ae363d1b0cdf9
Ibrahim Bali <balii@pm.me>
2023-07-13 15:20:47 +0300

    update pg_dump

--- useful-scripts/postgre/structure_dump.sh
+++ useful-scripts/postgre/structure_dump.sh
@@ -1,6 +1,20 @@
+host="hostname"
 username="postgres"
 port="5432"
-db_name="my_database_name"
+db_name="my_schema"
+table_name="public.my_table"
 db_dump_filename="./${db_name}.sql"
-pg_dump --host hostname --port ${port} --username ${username} --schema-only --verbose --file ${db_dump_filename} ${db_name}
+pg_dump \
+    --host ${host} \
+    --port ${port} \
+    --username ${username} \
+    --schema-only \
+    --verbose \
+    --table ${table_name} \
+    --file ${db_dump_filename} \
+    ${db_name}
 sed -i '' '/^--/d' ./${db_dump_filename}
+
+role="my_role"
+# fetch the lines including a certain string
+grep -E "(${role})" ${db_dump_filename} > "${db_name}.${role}.sql"
