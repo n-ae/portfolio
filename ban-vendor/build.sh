92185db6a5e9b998c0b144a3222fab424b3a77b6
bali-ibrahim <balii@pm.me>
2023-10-08 22:59:49 +0300

    feat(ban-vendor): add icon

--- ban-vendor/build.sh
+++ ban-vendor/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 dist_dir="dist"
-mkdir -p ${dist_dir}/assets
+mkdir -p ${dist_dir}/assets/icons
 
 cp manifest.json ${dist_dir}/
 cp popup.html ${dist_dir}/
-cp assets/* ${dist_dir}/assets/
+cp -r assets/* ${dist_dir}/assets/
 gopherjs build go/content_script.go -o ${dist_dir}/content_script.js
 gopherjs build go/service_worker.go -o ${dist_dir}/service_worker.js
