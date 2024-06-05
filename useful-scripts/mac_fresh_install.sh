ff309768b04f0a2ad807959d5dbe4d3f6b0f3601
bali <ibrahim.bali@forto.com>
2024-06-05 17:15:31 +0300

    feat: improve mac install

--- useful-scripts/mac_fresh_install.sh
+++ useful-scripts/mac_fresh_install.sh
@@ -5,7 +5,7 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update && brew upgrade
 
-brew install --cask firefox
+# brew install --cask firefox
 brew install --cask google-chrome
 brew install --cask adguard
 brew install --cask protonvpn
@@ -21,19 +21,25 @@ brew install docker
 brew install colima
 brew install --cask postman
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-. "$HOME/.cargo/env"
-# yew:
-rustup target add wasm32-unknown-unknown
-cargo install --locked trunk
-cargo install --locked wasm-bindgen-cli
-# yewN
+# rust & yew
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# . "$HOME/.cargo/env"
+# # yew:
+# rustup target add wasm32-unknown-unknown
+# cargo install --locked trunk
+# cargo install --locked wasm-bindgen-cli
+# # yewN
 
 # node:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 nvm install node # "node" is an alias for the latest version
 # node
 
-brew install --cask ngrok
-brew tap cargo-lambda/cargo-lambda
-brew install cargo-lambda
+# brew install --cask ngrok
+# brew tap cargo-lambda/cargo-lambda
+# brew install cargo-lambda
+
+# disable bottom right corner hover quick action
+defaults write com.apple.dock wvous-br-corner -int 0 && \
+defaults write com.apple.dock wvous-br-modifier -int 0 && \
+killall Dock
